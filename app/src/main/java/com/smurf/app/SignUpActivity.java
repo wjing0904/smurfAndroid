c02f046c764f4bf29461a900754017f8f77e5616
liuluchao <J4BKwRSiCg-c>
2020-10-10 16:05:21 +0800

    调试微信支付

--- app/src/main/java/com/smurf/app/SignUpActivity.java
+++ app/src/main/java/com/smurf/app/SignUpActivity.java
@@ -4,78 +4,53 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.smurf.app.webView.X5WebView;
+import com.tencent.smtt.export.external.interfaces.ConsoleMessage;
+import com.tencent.smtt.sdk.WebChromeClient;
 
 import java.util.HashMap;
 import java.util.Map;
-
 
 /**
  * 签约&& 认证
  */
 
 public class SignUpActivity extends Activity {
-    private WebView mWebView;
+    private X5WebView mWebView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_layout_webview);
+        setContentView(R.layout.layout_webview);
         String signUrl = getIntent().getStringExtra("sign_url");
-        mWebView = (WebView) findViewById(R.id.webview);
-        WebSettings webSetting = mWebView.getSettings();
-        webSetting.setJavaScriptEnabled(true);
-        webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSetting.setAllowFileAccess(true);
-        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-        webSetting.setSupportZoom(true);
-        webSetting.setBuiltInZoomControls(true);
-        webSetting.setUseWideViewPort(true);
-        webSetting.setSupportMultipleWindows(true);
-        // webSetting.setLoadWithOverviewMode(true);
-        webSetting.setAppCacheEnabled(true);
-        // webSetting.setDatabaseEnabled(true);
-        webSetting.setDomStorageEnabled(true);
-        webSetting.setGeolocationEnabled(true);
-        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
-        // webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
-        webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
-        // webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
-        webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webSetting.setAllowUniversalAccessFromFileURLs(true);
-        mWebView.setWebViewClient(new WebViewClient(){
+        mWebView = (X5WebView) findViewById(R.id.webview);
+        mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                String referer = "http://smurf.langongbao.com";
-                try {
-                    if (url.startsWith("weixin://") || url.startsWith("alipays://")) {
-                        Intent intent = new Intent();
-                        intent.setAction(Intent.ACTION_VIEW);
-                        intent.setData(Uri.parse(url));
-                        startActivity(intent);
-                        return true;
-                    }
-                }catch (Exception e){
-                    return false;
-                }
-
-                if(url.contains("https://wx.tenpay.com")){
-                    Map<String,String> extraHeaders = new HashMap<>();
-                    extraHeaders.put("Referer",referer);
-                    view.loadUrl(url,extraHeaders);
-                    referer = url;
-                    return true;
-                }
-                view.loadUrl(url);
-                return super.shouldOverrideUrlLoading(view, url);
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.i("consoleMessage", consoleMessage.message());
+                return super.onConsoleMessage(consoleMessage);
             }
         });
+        mWebView.getSettings();
+
+        if (signUrl.startsWith("weixin://") || signUrl.startsWith("alipays://")) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(signUrl));
+            startActivity(intent);
+            return;
+        }
+
+        if (signUrl.contains("https://wx.tenpay.com")) {
+            Map<String, String> extraHeaders = new HashMap<>();
+            extraHeaders.put("Referer", "http://smurf.langongbao.com");
+            mWebView.loadUrl(signUrl, extraHeaders);
+            return;
+        }
         mWebView.loadUrl(signUrl);
     }
 }
--- app/src/main/java/com/smurf/app/webView/X5WebView.java
+++ app/src/main/java/com/smurf/app/webView/X5WebView.java
@@ -15,50 +15,68 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class X5WebView extends WebView {
-	private WebViewClient client = new WebViewClient() {
-		/**
-		 * 防止加载网页时调起系统浏览器
-		 */
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			view.loadUrl(url);
-			return true;
-		}
-	};
+    private WebViewClient client = new WebViewClient() {
+        /**
+         * 防止加载网页时调起系统浏览器
+         */
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            try {
+                if (url.startsWith("weixin://") || url.startsWith("alipays://")) {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    getContext().startActivity(intent);
+                    return true;
+                }
+            } catch (Exception e) {
+                return false;
+            }
+            if (url.contains("https://wx.tenpay.com")) {
+                Map<String, String> extraHeaders = new HashMap<>();
+                extraHeaders.put("Referer", "http://smurf.langongbao.com");
+                view.loadUrl(url, extraHeaders);
+                return true;
+            }
 
-	@SuppressLint("SetJavaScriptEnabled")
-	public X5WebView(Context arg0, AttributeSet arg1) {
-		super(arg0, arg1);
-		this.setWebViewClient(client);
-		initWebViewSettings();
-		this.getView().setClickable(true);
-	}
+            view.loadUrl(url);
+            return true;
+        }
+    };
 
-	private void initWebViewSettings() {
-		WebSettings webSetting = this.getSettings();
-		webSetting.setJavaScriptEnabled(true);
-		webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
-		webSetting.setAllowFileAccess(true);
-		webSetting.setLayoutAlgorithm(LayoutAlgorithm.NARROW_COLUMNS);
-		webSetting.setSupportZoom(true);
-		webSetting.setBuiltInZoomControls(true);
-		webSetting.setUseWideViewPort(true);
-		webSetting.setSupportMultipleWindows(true);
-		// webSetting.setLoadWithOverviewMode(true);
-		webSetting.setAppCacheEnabled(true);
-		// webSetting.setDatabaseEnabled(true);
-		webSetting.setDomStorageEnabled(true);
-		webSetting.setGeolocationEnabled(true);
-		webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
-		// webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
-		webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
-		// webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
-		webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);
-		webSetting.setAllowUniversalAccessFromFileURLs(true);
-	}
+    @SuppressLint("SetJavaScriptEnabled")
+    public X5WebView(Context arg0, AttributeSet arg1) {
+        super(arg0, arg1);
+        this.setWebViewClient(client);
+        initWebViewSettings();
+        this.getView().setClickable(true);
+    }
 
-	public X5WebView(Context arg0) {
-		super(arg0);
-		setBackgroundColor(85621);
-	}
+    private void initWebViewSettings() {
+        WebSettings webSetting = this.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSetting.setAllowFileAccess(true);
+        webSetting.setLayoutAlgorithm(LayoutAlgorithm.NARROW_COLUMNS);
+        webSetting.setSupportZoom(true);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setUseWideViewPort(true);
+        webSetting.setSupportMultipleWindows(true);
+        // webSetting.setLoadWithOverviewMode(true);
+        webSetting.setAppCacheEnabled(true);
+        // webSetting.setDatabaseEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setGeolocationEnabled(true);
+        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
+        // webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
+        webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
+        // webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSetting.setAllowUniversalAccessFromFileURLs(true);
+    }
+
+    public X5WebView(Context arg0) {
+        super(arg0);
+        setBackgroundColor(85621);
+    }
 
 }
