4124521ca3110a4f39371174f9ccb25e8c062fa3
liuluchao0543@163.com <LLCH262724llch>
2020-10-25 13:05:15 +0800

    release 域名

--- app/src/main/java/com/smurf/app/StaticURL.java
+++ app/src/main/java/com/smurf/app/StaticURL.java
@@ -3,9 +3,9 @@ package com.smurf.app;
 public class StaticURL {
     //登陆
     public static final String DEBUG_PHONE_LOGIN = "http://39.107.84.57:8095/#/phoneLogin";
-    public static final String RELEASE_PHONE_LOGIN = "";
+    public static final String RELEASE_PHONE_LOGIN = "https://smurf.langongbao.com/#/phoneLogin";
 
     //首页
     public static final String DEBUG_APP_URL = "http://39.107.84.57:8095/#/home";
-    public static final String RELEASE_APP_URL = "";
+    public static final String RELEASE_APP_URL = "https://smurf.langongbao.com/#/home";
 }
