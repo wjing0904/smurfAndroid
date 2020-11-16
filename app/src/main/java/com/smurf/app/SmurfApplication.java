6e167f65b5966ee42cb63ce71e50ec53a4cbe9da
liuluchao <J4BKwRSiCg-c>
2020-11-16 18:29:11 +0800

    极光

--- app/src/main/java/com/smurf/app/SmurfApplication.java
+++ app/src/main/java/com/smurf/app/SmurfApplication.java
@@ -39,8 +39,8 @@ public class SmurfApplication  extends Application {
         JShareInterface.setDebugMode(true);
         PlatformConfig platformConfig = new PlatformConfig()
                 .setWechat("wx1bfb50c54805d042", "e5ba6b7e326639c6970199cf068f4cd5")
-                .setQQ("", "")
-                .setSinaWeibo("", "", "https://www.jiguang.cn");
+                .setQQ("101789350", "8bd761ec8be03a0c75477ad1d4eb2a03")
+                .setSinaWeibo("2906641376", "b495eedd2ac836895eb06c971e521073", "https://www.jiguang.cn");
         JShareInterface.init(this, platformConfig);
 
         QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
