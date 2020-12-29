54698022a7acfe0dca3a4329eb0fa64868518835
wangjing <wangjing@fescotech.com>
2020-12-29 16:10:08 +0800

    feature: 去除启动轮播，商城项目启动

--- base/src/main/java/com/smurf/app/base/StaticURL.java
+++ base/src/main/java/com/smurf/app/base/StaticURL.java
@@ -2,22 +2,22 @@ package com.smurf.app.base;
 
 public class StaticURL {
 
-    public static final String DEBUG_BASE = "http://39.107.84.57:8091/";
+    public static final String DEBUG_BASE = "http://39.107.84.57:8097/";
     public static final String RELEASE_BASE = "https://smurf.langongbao.com/";
 
-    public static final String DEBUG_BASE_QY = "http://39.107.84.57:8091/#/closeSign";
+    public static final String DEBUG_BASE_QY = "http://39.107.84.57:8097//#/closeSign";
     public static final String RELEASE_BASE_QY = "https://smurf.langongbao.com/#/closeSign";
 
     //登陆
-    public static final String DEBUG_PHONE_LOGIN = "http://39.107.84.57:8091/#/phoneLogin";
+    public static final String DEBUG_PHONE_LOGIN = "http://39.107.84.57:8097/#/phoneLogin";
     public static final String RELEASE_PHONE_LOGIN = "https://smurf.langongbao.com/#/phoneLogin";
 
     //首页
-    public static final String DEBUG_APP_URL = "http://39.107.84.57:8091/#/home";
-    public static final String RELEASE_APP_URL = "https://smurf.langongbao.com/#/home";
+    public static final String DEBUG_APP_URL = "http://39.107.84.57:8097/#/index";
+    public static final String RELEASE_APP_URL = "https://smurf.langongbao.com/#/index";
 
     //升级地址
-    public static final String DEBUG_APK_INSTALL_URL = "http://39.107.84.57:8090/api/sys/vno/detect";
+    public static final String DEBUG_APK_INSTALL_URL = "http://39.107.84.57:8093/api/sys/vno/detect";
     public static final String RELEASE_APK_INSTALL_URL = "https://api.langongbao.com/api/sys/vno/detect";
 
 
