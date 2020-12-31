18e52703c3547ad5f7ff00cf59856a259d004e92
liuluchao0543@163.com <LLCH262724llch>
2020-12-31 16:19:04 +0800

    登陆地址更换

--- base/src/main/java/com/smurf/app/base/StaticURL.java
+++ base/src/main/java/com/smurf/app/base/StaticURL.java
@@ -9,7 +9,7 @@ public class StaticURL {
     public static final String RELEASE_BASE_QY = "https://smurf.langongbao.com/#/closeSign";
 
     //登陆
-    public static final String DEBUG_PHONE_LOGIN = "http://39.107.84.57:8091/#/phoneLogin";
+    public static final String DEBUG_PHONE_LOGIN = "http://39.107.84.57:8097/#/phoneLogin";
     public static final String RELEASE_PHONE_LOGIN = "https://smurf.langongbao.com/#/phoneLogin";
 
     //首页
