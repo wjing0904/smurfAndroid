5f4f297a3ff3389e2361e29040954c7246f1f5b4
liuluchao0543@163.com <LLCH262724llch>
2020-12-31 16:07:54 +0800

    商城功能添加

--- base/src/main/java/com/smurf/app/base/StaticURL.java
+++ base/src/main/java/com/smurf/app/base/StaticURL.java
@@ -13,7 +13,8 @@ public class StaticURL {
     public static final String RELEASE_PHONE_LOGIN = "https://smurf.langongbao.com/#/phoneLogin";
 
     //首页
-    public static final String DEBUG_APP_URL = "http://39.107.84.57:8091/#/home";
+
+    public static final String DEBUG_APP_URL = "http://39.107.84.57:8097/#/index";
     public static final String RELEASE_APP_URL = "https://smurf.langongbao.com/#/home";
 
     //升级地址
